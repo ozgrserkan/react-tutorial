a61166b713bd3babc590e1c8ad221db4e3a72745
serkan <ozgr.serkan09@gmail.com>
2021-09-22 17:52:14 +0300

    useeffect basics,conditional,fetch data,multiple return and multiple return fetching data

--- react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020-master/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("call useEFFect");
+    if (value > 1) {
+      document.title = `Message (${value})`;
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log("hello world");
+  });
+
+  console.log("render component");
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        +
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,23 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
   return <h2>multiple returns</h2>;
 };
 
