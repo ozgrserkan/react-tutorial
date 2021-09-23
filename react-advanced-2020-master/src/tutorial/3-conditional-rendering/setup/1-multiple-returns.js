d127c4b9a137d9341e82abd85b125a8dd27fada7
serkan <ozgr.serkan09@gmail.com>
2021-09-23 18:08:30 +0300

    fetching data, short-circuit evaluation, ternary operator ,form basics

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -4,6 +4,25 @@ const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   if (isLoading) {
     return (
       <div>
@@ -18,7 +37,12 @@ const MultipleReturns = () => {
       </div>
     );
   }
-  return <h2>multiple returns</h2>;
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
