b233dab09ac58559a9b9e9bac1350b8769cf93ce
serkan <ozgr.serkan09@gmail.com>
2021-09-24 17:47:44 +0300

    show hide component, form basics, controlled inputs,add item to the list,multiple inputs

--- react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,9 +6,21 @@ import React, { useState } from "react";
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    if (firstName && email) {
+      const person = { firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
   };
   return (
     <React.Fragment>
@@ -16,17 +28,37 @@ const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name : </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email : </label>
-            <input type="text" id="firstName" name="email" />
+            <input
+              type="text"
+              id="firstName"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <button type="submit" onClick={handleSubmit}>
-            {" "}
             add person
           </button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={index}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </React.Fragment>
   );
