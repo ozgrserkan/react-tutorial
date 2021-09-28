91e30401992c63f21bfacd264f162ef8bb163d73
serkan <ozgr.serkan09@gmail.com>
2021-09-28 17:44:40 +0300

    multiple input useref, useref usestate setup,refactor,add item,remove item

--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,55 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === "TESTING");
+  {
+    return {
+      ...state,
+      people: data,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  throw new Error("no matching action type");
+};
+const defaultState = {
+  people: data,
+  isModalOpen: true,
+};
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      dispatch({ type: "Random " });
+    } else {
+    }
+  };
+  return (
+    <React.Fragment>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default Index;
