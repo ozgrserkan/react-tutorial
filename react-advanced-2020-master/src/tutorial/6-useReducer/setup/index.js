da3f71deae797fe57cd7d87f326bab9ea27d08db
serkan <ozgr.serkan09@gmail.com>
2021-09-29 17:52:52 +0300

    use reducer, prop drilling, contex api , use context custom hooks, use context

--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -1,21 +1,11 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
+import { reducer } from "./reducer";
 // reducer function
-const reducer = (state, action) => {
-  if (action.type === "TESTING");
-  {
-    return {
-      ...state,
-      people: data,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-  throw new Error("no matching action type");
-};
+
 const defaultState = {
-  people: data,
+  people: [],
   isModalOpen: true,
 };
 const Index = () => {
@@ -24,13 +14,21 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      dispatch({ type: "Random " });
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName("");
     } else {
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
   return (
     <React.Fragment>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -43,8 +41,15 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              Remove
+            </button>
           </div>
         );
       })}
--- react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2020-master/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+const [loading, setLoading] = useState(true);
+const [products, setProducts] = useState([]);
 
+const getProducts = async () => {
+  const response = await fetch(url);
+  const products = await response.json();
+  setProducts(products);
+  setLoading(false);
+};
+
+useEffect(() => {
+  getProducts();
+}, []);
 export const useFetch = () => {};
+return { loading, products };
